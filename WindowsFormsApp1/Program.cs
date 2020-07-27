30c4508ba0f573a3a2013a0bc2aedaebe7ba0497
0796myhome <ava8515@163.com>
2020-07-27 15:40:25 +0800

    4

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,8 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
